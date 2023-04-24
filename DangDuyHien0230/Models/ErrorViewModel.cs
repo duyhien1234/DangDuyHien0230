24c95055957126a2d3c82f730761a766aa351812
Tên_của_mình <1921050230@student.humg.edu.vn>
2023-04-24 15:14:31 +0700

    baikiemtra

---
+++ DangDuyHien0230/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DangDuyHien0230.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
